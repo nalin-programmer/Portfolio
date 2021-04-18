c3d44c5cb694df6c3da817288e15a495a58f66bf
NALIN AGRAWAL <54065357+nalin-programmer@users.noreply.github.com>
2021-04-18 19:07:46 +0530

    errors removed

--- src/data/SkillsData.js
+++ src/data/SkillsData.js
@@ -2,26 +2,12 @@ const skills = {
   skillsArray: [
     {
       _id: "1",
-      icon: (
-        <span
-          class="iconify HomeScreenSkillContentIcon"
-          data-icon="file-icons:c"
-          data-inline="false"
-          style={{ height: "28px", width: "28px" }}
-        ></span>
-      ),
+      icon: <i class="fab icon-cplusplus fa-2x HomeScreenSkillContentIcon"></i>,
       name: "C++",
     },
     {
       _id: "2",
-      icon: (
-        <span
-          class="iconify HomeScreenSkillContentIcon"
-          data-icon="teenyicons:c-solid"
-          data-inline="false"
-          style={{ height: "28px", width: "28px" }}
-        ></span>
-      ),
+      icon: <i class="fab icon-c fa-2x HomeScreenSkillContentIcon"></i>,
       name: "C",
     },
     {
@@ -37,12 +23,17 @@ const skills = {
     {
       _id: "5",
       icon: (
-        <span
-          class="iconify"
-          data-icon="carbon:data-structured"
-          data-inline="false"
-          style={{ height: "28px", width: "28px" }}
-        ></span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="32"
+          height="32"
+          fill="currentColor"
+          class="bi bi-file-earmark-binary-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M5.526 10.273c-.542 0-.832.563-.832 1.612 0 .088.003.173.006.252l1.559-1.143c-.126-.474-.375-.72-.733-.72zm-.732 2.508c.126.472.372.718.732.718.54 0 .83-.563.83-1.614 0-.085-.003-.17-.006-.25l-1.556 1.146z" />
+          <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zm-2.45 8.385c0 1.415-.548 2.206-1.524 2.206C4.548 14.09 4 13.3 4 11.885c0-1.412.548-2.203 1.526-2.203.976 0 1.524.79 1.524 2.203zm3.805 1.52V14h-3v-.595h1.181V10.5h-.05l-1.136.747v-.688l1.19-.786h.69v3.633h1.125z" />
+        </svg>
       ),
       name: "DSA",
     },
@@ -75,26 +66,12 @@ const skills = {
     },
     {
       _id: "10",
-      icon: (
-        <span
-          class="iconify HomeScreenSkillContentIcon"
-          data-icon="cib:mongodb"
-          data-inline="false"
-          style={{ height: "28px", width: "28px" }}
-        ></span>
-      ),
+      icon: <i class="fab icon-mongodb fa-2x HomeScreenSkillContentIcon"></i>,
       name: "MongoDB",
     },
     {
       _id: "11",
-      icon: (
-        <span
-          class="iconify HomeScreenSkillContentIcon"
-          data-icon="grommet-icons:mysql"
-          data-inline="false"
-          style={{ height: "28px", width: "28px" }}
-        ></span>
-      ),
+      icon: <i class="fab icon-mysql-alt fa-2x HomeScreenSkillContentIcon"></i>,
       name: "MySQL",
     },
     {
@@ -130,6 +107,11 @@ const skills = {
       name: "CSS",
     },
     {
+      _id: "16",
+      icon: <i class="fab icon-sass fa-2x HomeScreenSkillContentIcon"></i>,
+      name: "SASS",
+    },
+    {
       _id: "17",
       icon: <i class="fab fa-github fa-2x HomeScreenSkillContentIcon"></i>,
       name: "Github",
@@ -141,14 +123,7 @@ const skills = {
     },
     {
       _id: "19",
-      icon: (
-        <span
-          class="iconify HomeScreenSkillContentIcon"
-          data-icon="cib:heroku"
-          data-inline="false"
-          style={{ height: "28px", width: "28px" }}
-        ></span>
-      ),
+      icon: <i class="fab icon-heroku fa-2x HomeScreenSkillContentIcon"></i>,
       name: "Heroku",
     },
   ],
