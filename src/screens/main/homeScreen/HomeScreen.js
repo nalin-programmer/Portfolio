fc42ac282af87240cd589b09057742e0e7f397c1
Nalin Agrawal <54065357+nalin-programmer@users.noreply.github.com>
2022-04-18 15:38:18 +0530

    major changes in intro part

--- src/screens/main/homeScreen/HomeScreen.js
+++ src/screens/main/homeScreen/HomeScreen.js
@@ -2,16 +2,30 @@ import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Nalin from "../../../images/NALIN.png";
+import Background from "../../../images/Background.png";
 import Resume from "../../../data/Resume.pdf";
 import Button from "@material-ui/core/Button";
 import "./HomeScreen.css";
 import Fade from "react-reveal/Fade";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 import RubberBand from "react-reveal/RubberBand";
+import Grid from "@material-ui/core/Grid";
 export default function HomeScreen() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 400,
+      backSpeed: 60,
+      strings: ["MERN Stack Web Developer", "Competitive Programmer"],
+    });
+  }, []);
   return (
     <div>
       <Fade bottom>
-        <Jumbotron fluid className="HomeScreenJumbotron">
+        {/* <Jumbotron fluid className="HomeScreenJumbotron">
           <Container className="HomeScreenSmallIntro">
             <img src={Nalin} className="HomeSceenProfilePicture" />
             <h1 className="HomeScreenH1">Nalin Agrawal</h1>
@@ -26,7 +40,48 @@ export default function HomeScreen() {
               grip on Data Structures and Algorithm.
             </p>
           </Container>
-        </Jumbotron>
+        </Jumbotron> */}
+        <Grid container>
+          <Grid
+            style={{ textAlign: "center" }}
+            item
+            xs={12}
+            sm={6}
+            md={6}
+            lg={6}
+          >
+            <div className="imgContainer">
+              <img
+                src={Nalin}
+                alt=""
+                style={{
+                  hight: "250px",
+                  width: "250px",
+                  borderRadius: "50%",
+                  backgroundColor: "#1E90FF",
+                }}
+              />
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <div style={{ width: "100%", padding: "50px 0", height: "100%" }}>
+              <h2 className="intro_text">Hi I'm </h2>
+              {/* <h1 className="intro_text">Nalin Agrawal</h1> */}
+              <h1
+                style={{
+                  fontSize: "3rem",
+                  fontWeight: "bold",
+                  marginBottom: "15px",
+                }}
+              >
+                Nalin Agrawal
+              </h1>
+              <h3 className="intro_text">
+                I'm a <span style={{ color: "#3f51b5" }} ref={textRef}></span>
+              </h3>
+            </div>
+          </Grid>
+        </Grid>
       </Fade>
       <RubberBand>
         <div className="homeScreenPdfContainer">
