584b1172b48eacb20c8a492872f5331b9d3be790
Nalin Agrawal <you@example.comnalinagrawal333@gmail.com>
2022-04-19 15:41:39 +0530

    added photos to projects

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -4,6 +4,7 @@ const projects = {
       _id: "1",
       title: "Lococart",
       languages: "React.JS | Node.JS | Express.JS | MongoDB",
+      images: "/images/lococart.png",
       description:
         " A bidding-based ecommerce project in MERN stack.  Customers add their shopping list and seller bid on it by default, seller with minimum price gets the order. All sellers within a local area can bid on the order within 3 hours bidding window. It ensures that customers can get best prices on products they want. ",
       gitHubUrl: "https://github.com/BTP-Project-Semester-4",
@@ -13,6 +14,7 @@ const projects = {
       _id: "2",
       title: "Donation-Website-MERN",
       languages: "React.JS | Node.JS | Express.JS | MongoDB",
+      images: "/images/sahayata.png",
       description:
         "Developed in MERN stack, it is an online donation  website. Here people who want to donate things will add them to website and those people who need them can request. It is the choice of person giving the donation to accept it or not. It will make sure that real needy person gets the donation as per the will of the donor ",
       gitHubUrl: "https://github.com/nalin-programmer/Donation-Website-MERN",
@@ -22,6 +24,7 @@ const projects = {
       _id: "3",
       title: "Virtual-Interview-Platform",
       languages: "React.JS | Node.JS | MySQL",
+      images: "/images/vip.png",
       description:
         "Its a virtual platform providing with live interviews, chat box, result portal, etc. Designed to fulfill the modern day interview requirements all in one website!!! And fulfills all needs of both the candidate and interviewer.",
       gitHubUrl: "https://github.com/BTP-Project/VIrtual-Interview-Hosting",
@@ -30,6 +33,7 @@ const projects = {
     {
       _id: "4",
       title: "Instagram Clone",
+      images: "/images/instagramClone.jpg",
       languages: "Java | Android Studio | Firebase",
       description:
         "This is clone of famous app Instagram which I made in Java using Android Studio and Firebase, and has all features that Instagram can support like posts, notification, live status, etc. ",
@@ -39,15 +43,17 @@ const projects = {
     {
       _id: "5",
       title: "Web Chat",
+      images: "/images/WebChat.jpg",
       languages: "Node.Js | Express.Js | Socket.io",
       description:
         "This is an online video conferencing web page made with NodeJS and WebRTC used for video chat between two people. ",
-      gitHubUrl: "https://github.com/nalin-programmer/InstagramClone",
+      gitHubUrl: "https://github.com/nalin-programmer/WebChat",
       hostedUrl: "https://video-confrencing.herokuapp.com/",
     },
     {
       _id: "6",
       title: "Desktop-Assistant",
+      images: "/images/DesktopAssistant.png",
       languages: "Python",
       description:
         "This is a desktop assistant created in python language. And take voice input and give output in voice and text form.  ",
@@ -57,15 +63,17 @@ const projects = {
     {
       _id: "7",
       title: "Movie Recomendation System",
+      images: "/images/MovieRecomendation.png",
       languages: "React.JS | Node.JS | Express.JS | MongoDB | Flask",
       description:
-        "(Small Project)It is a movie recomending system which not only shows trending movies, categories of movies but also gives recommendations on movies based on your movie choices.",
+        "It is a movie recomending system which not only shows trending movies, categories of movies but also gives recommendations on movies based on your movie choices.",
       gitHubUrl: "https://github.com/SummerProject2020-21",
       hostedUrl: "",
     },
     {
       _id: "8",
       title: "Fast food ordering and billing system",
+      images: "/images/FastFood.png",
       languages: "C++",
       description:
         "It's a C++ code in which we can order fast food and then it also returns bill of it.",
@@ -76,18 +84,20 @@ const projects = {
     {
       _id: "9",
       title: "BloothFinder",
+      images: "/images/BluetoothFinder.jpg",
       languages: "Java | Android Studio",
       description:
-        "(Small Project)This is a simple app that detects bluetooth devices around it.",
+        "This is a simple app that detects bluetooth devices around it.",
       gitHubUrl: "https://github.com/nalin-programmer/BloothFinder",
       hostedUrl: "",
     },
     {
       _id: "10",
       title: "NotesApp",
+      images: "/images/AddNewNote.jpg",
       languages: "Java | Android Studio",
       description:
-        "(Small Project)This is an app that saves multiple multiline notes in offline manner.",
+        "This is an app that saves multiple multiline notes in offline manner.",
       gitHubUrl: "https://github.com/nalin-programmer/NotesApp",
       hostedUrl: "",
     },
